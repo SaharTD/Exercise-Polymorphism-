f7a14b1f0f5bfeaf483254a5fd7e991559a3b3ce
Sahar Turki <103968284+SaharTD@users.noreply.github.com>
2025-03-02 11:19:07 -0900

    Update Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,6 +1,9 @@
 public interface Vehicle {
 
-
+int hours_rate=10;/// for the Bike
+    int days_rate=50;///for the car
+    int week_rate=500;/// and for the truck
+    
     double calculateRentalCost();
     void displayDetails();
 
